fd821fe788444e52d60abd276d47c73c88f009d9
Behrad Rajabiefumani <behrad.rajabie@gmail.com>
2019-08-04 13:30:14 +0200

    .

--- start.js
+++ start.js
@@ -1,9 +1,8 @@
 //@ts-check
+'use strict';
 function sideBarOpen(){
     document.getElementById("sidebar").style.width = "200px";
-    document.getElementById("grid").style.marginRight = "200px";
 }
 function sideBarClose(){
     document.getElementById("sidebar").style.width = "0px";
-    document.getElementById("grid").style.marginRight = "0px";
 }
